49e0e9161d1ce8e26ac3bd07b97d27c769d46590
Mueed98 <mueed98@github.com>
2020-10-05 18:56:13 +0500

    Create object.cpp

---
+++ object.cpp
@@ -0,0 +1,23 @@
+/*
+ * object.cpp
+ *
+ *  Created on: May 10, 2018
+ *      Author: mueed
+ */
+
+#include "object.h"
+
+object::object() {
+	// TODO Auto-generated constructor stub
+cout<<"Object  created"<<endl;
+}
+
+object::~object() {
+	// TODO Auto-generated destructor stub
+	cout<<"Object  destroyed"<<endl;
+}
+void object::display()
+{
+	cout<<"Object  Displayed"<<endl;
+}
+
