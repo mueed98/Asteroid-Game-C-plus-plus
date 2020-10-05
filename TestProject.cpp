c0175a21a9a40094696308358ae7b4d50fc5103a
Mueed98 <mueed98@github.com>
2020-10-05 18:56:23 +0500

    Create TestProject.cpp

---
+++ TestProject.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : TestProject.cpp
+// Author      : i170132
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C, Ansi-style
+//============================================================================
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Section D");
+	return EXIT_SUCCESS;
+}
